0c7d012548eaa2d4c26ad586b251f68d4f7c955f
James Magary <jammag3147@gmail.com>
2015-06-21 17:42:00 -0400

    Complete
    Complete functions and comments.  Contains usage example at top.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,28 @@
 ##Functions that cache the inverse of a matrix
+##
+##Example:
+##  source('cachematrix.R')
+##  v <- c(3,4,1,2)
+##  a <- makeCacheMatrix(matrix(v, c(2,2)))
+##  cacheSolve(a)
+##          [,1] [,2]
+##    [1,]    1 -0.5
+##    [2,]   -2  1.5
+##  cacheSolve(a)
+##    Getting Cached Data
+##          [,1] [,2]
+##    [1,]    1 -0.5
+##    [2,]   -2  1.5
 
-##This function creates a special "matrix" object that can cache its inverse
-##Create matrix, which is a list containing functions
+
+
+
+##  This function creates a special "matrix" object, which is a list containing functions to:
+##    set the value of the matrix (set)
+##    get the value of the matrix (get)
+##    set the value of the inverse matrix (setinverse)
+##    get the value of the inverse matrix (getinverse)
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   
@@ -24,17 +45,18 @@ makeCacheMatrix <- function(x = matrix()) {
   )
 }
 
-##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
-##If the inverse has already been calculated (and the matrix has not changed),
-##then the cachesolve should retrieve the inverse from the cache.
-##Calculate inverse of the matrix created with above function, reusing cached result if available
+
+
+##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+##  If the inverse has already been calculated (and the matrix has not changed),
+##    then cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  ##Return a matrix that is the inverse of 'x'
+  ##Return a matrix that is the inverse of "x"
   i <- x$getinverse()
-  
+  ##Checks for cached data - prints message and uses data if found
   if(!is.null(i)) {
-    message("getting cached data")
+    message("Getting Cached Data")
     return(i)
   }
   
